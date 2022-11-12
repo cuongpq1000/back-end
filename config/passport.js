08904642a305c0e6cb017b71b2e3245ac3dac1c3
khanh19 <khanh02092000@gmail.com>
2022-11-12 16:48:31 -0500

    google authentication

---
+++ config/passport.js
@@ -0,0 +1,50 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('../model/User')
+const config = require('../config/config.json')
+module.exports = function (passport) {
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: config['google-client-id'],
+                clientSecret: config['google-client-secret'],
+                callbackURL: '/auth/google/callback',
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                //get the user data from google 
+                const newUser = {
+                    googleId: profile.id,
+                    displayName: profile.displayName,
+                    firstName: profile.name.givenName,
+                    lastName: profile.name.familyName,
+                    image: profile.photos[0].value,
+                    email: profile.emails[0].value
+                }
+
+                try {
+                    //find the user in our database 
+                    let user = await User.findOne({ googleId: profile.id })
+
+                    if (user) {
+                        //If user present in our database.
+                        done(null, user)
+                    } else {
+                        // if user is not preset in our database save user data to database.
+                        user = await User.create(newUser)
+                        done(null, user)
+                    }
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+        ))
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
+
+    // used to deserialize the user
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => done(err, user))
+    })
+}
--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.port || 3030;
+const config = require('./config/config.json')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+mongoose.connect(config.connectionString, {
+    useNewUrlParser: true
+})
 
 
-app.listen(port, () =>{
+require('./config/passport')(passport)
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.use(require("./router/index.router"))
+app.use('/auth', require('./router/auth.router'))
+
+app.listen(port, () => {
     console.log("listen to " + port);
 })
+
